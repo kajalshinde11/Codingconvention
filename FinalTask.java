b32911cabf71c2531499b09bdf94c2676f4de5d8
kajalshinde11 <79559030+kajalshinde11@users.noreply.github.com>
2021-02-26 04:48:55 -0800

    Create FinalTask.java

---
+++ FinalTask.java
@@ -0,0 +1,30 @@
+public class employee 
+{
+    int id;
+    double salary=0.0;
+
+    public employee(int Id) {
+        this.Id = id;
+    }
+
+    public employee(){
+    } 
+
+    double calTax(){
+     if(this.Salary > 500000.00 && this.salary < 1000000)
+        return this.salary*0.1;
+     else if(this.salary > 1000000.00 && this.Salary < 1500000) 
+         return this.Salary*0.2;
+     else      
+         return this.Salary*0.3;       
+
+     for(int index=0;index<10;index++){
+
+     }
+   }
+}     
+                         
+                                   
+
+      
+                      
